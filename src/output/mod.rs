ebbac61c742aad0eb4e36ed25c7f7a2300106791
Ben S <ogham@bsago.me>
2015-08-03 10:22:24 +0100

    Slim down symlink arrow
    Fixes #74. There's one argument for changing the arrow, and none against!

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -28,13 +28,13 @@ fn symlink_filename(file: &File, colours: &Colours) -> String {
     match file.link_target() {
         Ok(target) => format!("{} {} {}",
                               file_colour(colours, file).paint(&file.name),
-                              colours.punctuation.paint("=>"),
+                              colours.punctuation.paint("->"),
                               ANSIStrings(&[ colours.symlink_path.paint(&target.path_prefix()),
                                              file_colour(colours, &target).paint(&target.name) ])),
 
         Err(filename) => format!("{} {} {}",
                                  file_colour(colours, file).paint(&file.name),
-                                 colours.broken_arrow.paint("=>"),
+                                 colours.broken_arrow.paint("->"),
                                  colours.broken_filename.paint(&filename)),
     }
 }
