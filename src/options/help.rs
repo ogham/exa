89a6e14e154a987b742423bba238ea8ed174017e
James Reed <jcrd@tuta.io>
2018-10-26 17:04:22 -0600

    Move level option to correct group

--- src/options/help.rs
+++ src/options/help.rs
@@ -23,6 +23,7 @@ DISPLAY OPTIONS
 FILTERING AND SORTING OPTIONS
   -a, --all                  show hidden and 'dot' files
   -d, --list-dirs            list directories like regular files
+  -L, --level DEPTH          limit the depth of recursion
   -r, --reverse              reverse the sort order
   -s, --sort SORT_FIELD      which field to sort by
   --group-directories-first  list directories before other files
@@ -41,7 +42,6 @@ LONG VIEW OPTIONS
   -h, --header       add a header row to each column
   -H, --links        list each file's number of hard links
   -i, --inode        list each file's inode number
-  -L, --level DEPTH  limit the depth of recursion
   -m, --modified     use the modified timestamp field
   -S, --blocks       show number of file system blocks
   -t, --time FIELD   which timestamp field to list (modified, accessed, created)
