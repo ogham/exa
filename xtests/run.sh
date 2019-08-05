3247de3cbb1c7c4a205604d08a26b5a6c60db246
Laurent Arnoud <laurent@spkdev.net>
2019-08-05 22:47:52 +0200

    Fix vagrant integration tests
    * For dates need the LC_ALL env
    * Missing new option --icons on help output

--- xtests/run.sh
+++ xtests/run.sh
@@ -153,8 +153,8 @@ $exa $testcases/dates -l       --time-style=iso      2>&1 | diff -q - $results/d
 # Locales
 # These two are used in particular because they have 5-long and 4-long
 # month names respectively
-env LANG=fr_FR.UTF-8  $exa $testcases/dates -l | diff -q - $results/dates_fr  || exit 1
-env LANG=ja_JP.UTF-8  $exa $testcases/dates -l | diff -q - $results/dates_jp  || exit 1
+env LC_ALL=fr_FR.UTF-8 LANG=fr_FR.UTF-8 $exa $testcases/dates -l | diff -q - $results/dates_fr  || exit 1
+env LC_ALL=ja_JP.UTF-8 LANG=ja_JP.UTF-8 $exa $testcases/dates -l | diff -q - $results/dates_jp  || exit 1
 
 
 # Paths and directories
