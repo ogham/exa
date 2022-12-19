3184a7f2e6a460e9b4cbe940649b3970d51138d6
Marco Radocchia <marco.radocchia@outlook.com>
2022-12-19 18:00:33 +0100

    Fixed issue #1106: added icon for '.gpg' files

--- src/output/icons.rs
+++ src/output/icons.rs
@@ -191,6 +191,7 @@ pub fn icon_for_file(file: &File<'_>) -> char {
             "gitignore"     => '\u{f1d3}', // 
             "gitmodules"    => '\u{f1d3}', // 
             "go"            => '\u{e626}', // 
+            "gpg"           => '\u{ea75}', // 
             "gradle"        => '\u{e256}', // 
             "groovy"        => '\u{e775}', // 
             "gsheet"        => '\u{f1c3}', // 
