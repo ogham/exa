58dcccdd48a8110fa22daa20cab28af65722fabf
DenebTM <deneb@screee.ee>
2023-02-24 23:15:01 +0100

    Clean up the non-printable loop somewhat

--- src/output/escape.rs
+++ src/output/escape.rs
@@ -13,16 +13,13 @@ pub fn escape(string: String, bits: &mut Vec<ANSIString<'_>>, good: Style, bad: 
             // The `escape_default` method on `char` is *almost* what we want here, but
             // it still escapes non-ASCII UTF-8 characters, which are still printable.
 
+            // TODO: This allocates way too much,
+            // hence the `all` check above.
             if c >= 0x20 as char && c != 0x7f as char {
-                // TODO: This allocates way too much,
-                // hence the `all` check above.
-                let mut s = String::new();
-                s.push(c);
-                bits.push(good.paint(s));
+                bits.push(good.paint(c.to_string()));
             }
             else {
-                let s = c.escape_default().collect::<String>();
-                bits.push(bad.paint(s));
+                bits.push(bad.paint(c.escape_default().to_string()));
             }
         }
     }
