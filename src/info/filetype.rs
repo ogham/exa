846f4ddf6305ba6535f5fd830d6bbb31e9792dd4
aarondill <aaronsacks2006@gmail.com>
2023-07-24 00:51:59 -0500

    add webpack.config.cjs to immediate files list
    (.mjs is excluded bc webpack doesn't currently support it)

--- src/info/filetype.rs
+++ src/info/filetype.rs
@@ -27,9 +27,9 @@ impl FileExtensions {
             "Makefile", "Cargo.toml", "SConstruct", "CMakeLists.txt",
             "build.gradle", "pom.xml", "Rakefile", "package.json", "Gruntfile.js",
             "Gruntfile.coffee", "BUILD", "BUILD.bazel", "WORKSPACE", "build.xml", "Podfile",
-            "webpack.config.js", "meson.build", "composer.json", "RoboFile.php", "PKGBUILD",
-            "Justfile", "Procfile", "Dockerfile", "Containerfile", "Vagrantfile", "Brewfile",
-            "Gemfile", "Pipfile", "build.sbt", "mix.exs", "bsconfig.json", "tsconfig.json",
+            "webpack.config.js", "webpack.config.cjs", "meson.build", "composer.json", "RoboFile.php",
+            "PKGBUILD", "Justfile", "Procfile", "Dockerfile", "Containerfile", "Vagrantfile",
+            "Brewfile", "Gemfile", "Pipfile", "build.sbt", "mix.exs", "bsconfig.json", "tsconfig.json",
         ])
     }
 
