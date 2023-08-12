818cd8f4bf6c2e9b32c9d0247b5efab88a8460d9
Sandro-Alessio Gierens <sandro@gierens.de>
2023-08-12 12:51:23 +0200

    Add gitlab-ci.yml as intermediate file with Gitlab icon

--- src/output/icons.rs
+++ src/output/icons.rs
@@ -69,6 +69,7 @@ lazy_static! {
         m.insert("Dockerfile", '\u{f308}'); // 
         m.insert("ds_store", '\u{f179}'); // 
         m.insert("gitignore_global", '\u{f1d3}'); // 
+        m.insert("gitlab-ci.yml", '\u{f296}'); // 
         m.insert("go.mod", '\u{e626}'); // 
         m.insert("go.sum", '\u{e626}'); // 
         m.insert("gradle", '\u{e256}'); // 
